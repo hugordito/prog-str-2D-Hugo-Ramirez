ae053d6690d88a3c1ccb4bf5cceeeae9d06ff38b
hugordito <hugoalberto2257@gmail.com>
2026-01-13 19:46:00 -0600

    Proyecto 01: hola mundo

---
+++ proyecto-01-hola-mundo/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hugo Alberto Ramirez Martinez");
+        System.out.println("2D");
+        System.out.println("13 de Enero del 2026");
+        System.out.println("Hola mundo");
+        }
+    }
+}
