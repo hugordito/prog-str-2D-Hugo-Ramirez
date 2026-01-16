a3ec33fd270e2501ec1920058d3632a560f5e8d4
Hugordito <hugoalberto2257@gmail.com>
2026-01-16 08:12:24 -0600

    sse crea rama dev y se edita .gitnore

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # prog-str-2D-hugo-ramirez
+#este es el repositorio del cuatrimestre, aqui vamos a poner todos los trabajos
--- proyecto-01-hola-mundo/src/Main.java
+++ proyecto-01-hola-mundo/src/Main.java
@@ -1,5 +1,5 @@
 
-public class Main {
+public class cheMain {
     public static void main(String[] args) {
         System.out.println("Hugo Alberto Ramirez Martinez");
         System.out.println("2D");
